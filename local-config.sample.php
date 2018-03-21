3c8429780a7228b9e81bff83308d416e2a946512
aiziyuer <ziyu0123456789@gmail.com>
2018-03-21 09:57:40 -0400

    首次提交

---
+++ local-config.sample.php
@@ -0,0 +1,12 @@
+<?php
+/*
+This is a sample local-config.php file
+In it, you *must* include the four main database defines
+
+You may include other settings here that you only want enabled on your local development checkouts
+*/
+
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wpuser' );
+define( 'DB_PASSWORD', 'wppassword' );
+define( 'DB_HOST', 'localhost' ); // Probably 'localhost'
